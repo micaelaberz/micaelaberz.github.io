ad9b3995728d9b8e473f401e66c57896616161dd
Micaela <micaelaberzoni01@gmail.com>
2024-09-24 00:24:04 -0300

    ejercicio 2 especiales

--- Especiales/json_flores.js
+++ Especiales/json_flores.js
@@ -1,5 +1,5 @@
-var textoFlores = '{"flores": [{"codigo": "FL001","nombre": "Rosa","familia": "Rosaceae","color": "Rojo","fecha_floracion": "2024-04-15"}, ' +
-    '{"codigo": "FL002","nombre": "Girasol","familia": "Asteraceae","color": "Amarillo","fecha_floracion": "2024-07-10"}' +
-    ',{"codigo": "FL003","nombre": "Orquídea","familia": "Orchidaceae","color": "Variado","fecha_floracion": "2024-05-20"}'+
-    ',{"codigo": "FL004","nombre": "Tulipán","familia": "Liliaceae","color": "Varios","fecha_floracion": "2024-03-25"}' +
-    ', {"codigo": "FL005", "nombre": "Lirio", "familia": "Liliaceae", "color": "Blanco", "fecha_floracion": "2024-06-05"}]}';
+var textoFlores = '{"flores": [{"codigo": "001","nombre": "Rosa","familia": "Rosaceae","color": "Rojo","fecha_floracion": "2024-04-15"}, ' +
+    '{"codigo": "002","nombre": "Girasol","familia": "Asteraceae","color": "Amarillo","fecha_floracion": "2024-07-10"}' +
+    ',{"codigo": "003","nombre": "Orquídea","familia": "Orchidaceae","color": "Variado","fecha_floracion": "2024-05-20"}'+
+    ',{"codigo": "004","nombre": "Tulipán","familia": "Liliaceae","color": "Varios","fecha_floracion": "2024-03-25"}' +
+    ', {"codigo": "005", "nombre": "Lirio", "familia": "Liliaceae", "color": "Blanco", "fecha_floracion": "2024-06-05"}]}';
---
+++ Especiales/venta_flores.js
@@ -0,0 +1,17 @@
+var venta = '{"flor": [' +
+    '{"codigo": "FL001","nombre": "Rosa","tipo": "Perene","color": "Rojo","cantidad": 10}, ' +
+    '{"codigo": "FL002","nombre": "Girasol","tipo": "Anual","color": "Amarillo","cantidad": 15}, ' +
+    '{"codigo": "FL003","nombre": "Orquídea","tipo": "Perene","color": "Variado","cantidad": 8}, ' +
+    '{"codigo": "FL004","nombre": "Tulipán","tipo": "Anual","color": "Varios","cantidad": 12}, ' +
+    '{"codigo": "FL005","nombre": "Lirio","tipo": "Perene","color": "Blanco","cantidad": 20}, ' +
+    '{"codigo": "FL006","nombre": "Clavel","tipo": "Perene","color": "Rosa","cantidad": 18}, ' +
+    '{"codigo": "FL007","nombre": "Magnolia","tipo": "Perene","color": "Blanco","cantidad": 5}, ' +
+    '{"codigo": "FL008","nombre": "Azalea","tipo": "Perene","color": "Rojo","cantidad": 14}, ' +
+    '{"codigo": "FL009","nombre": "Violeta","tipo": "Anual","color": "Morado","cantidad": 7}, ' +
+    '{"codigo": "FL010","nombre": "Dalia","tipo": "Anual","color": "Multicolor","cantidad": 9}, ' +
+    '{"codigo": "FL011","nombre": "Gerbera","tipo": "Anual","color": "Naranja","cantidad": 11}, ' +
+    '{"codigo": "FL012","nombre": "Hortensia","tipo": "Perene","color": "Azul","cantidad": 4}, ' +
+    '{"codigo": "FL013","nombre": "Crisantemo","tipo": "Anual","color": "Blanco","cantidad": 6}, ' +
+    '{"codigo": "FL014","nombre": "Peonía","tipo": "Perene","color": "Rosa","cantidad": 3}, ' +
+    '{"codigo": "FL015","nombre": "Fresia","tipo": "Anual","color": "Amarillo","cantidad": 8}' +
+    ']}';
